caba148fe2afcb27d5b2749f9bb5611abc1a701c
Lindsey Halliday and Matt Jackson <lindsey@>
2017-12-06 20:31:04 -0800

    Add more functionality to game.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 function Play(roll, currentScore, totalScore) {
   this.roll = roll;
   this.currentScore = currentScore;
-  this.hold = totalScore;
+  this.totalScore = totalScore;
   if (totalScore >= 100) {
     alert("You Won!");
   }
@@ -16,35 +16,55 @@ Play.prototype.diceRoll = function() {
 
 Play.prototype.fullTurn = function(roll) {
   if (roll === 1) {
-    this.currentScore === 0;
+    this.currentScore = 0;
   }
   else {
     this.currentScore += roll;
   }
 }
 
+Play.prototype.Hold = function() {
+  this.currentScore += this.totalScore;
+}
 
 
 //UI Logic
 $(document).ready(function() {
   var Player1 = new Play(0, 0, 0);
+  var Player2 = new Play(0, 0, 0);
   $("#btn-roll").click(function(event) {
+    console.log(Player1);
     event.preventDefault();
-    // var myRoll = new Roll (roll, currentScore, totalScore);
-    //   $(".your-roll").append(myRoll.roll);
-    //   $(".current-score").append(myRoll.currentScore);
-    //   $(".total-score").append(myRoll.totalScore);
-    //$(".ul-result").append(Play);
     Player1.diceRoll();
     Player1.fullTurn(Player1.roll);
     $(".your-roll").text(Player1.roll);
     $(".current-score").text(Player1.currentScore);
-    console.log(Player1.roll);
-
+    if (Player1.roll ===  1) {
+      $(".current-score").text(Player1.currentScore);
+    }
   });
+
+  $("#btn-roll2").click(function(event) {
+    event.preventDefault();
+    Player2.diceRoll();
+    Player2.fullTurn(Player2.roll);
+    $(".your-roll2").text(Player2.roll);
+    $(".current-score2").text(Player2.currentScore);
+    if (Player2.roll ===  1) {
+      $(".current-score2").text(Player2.currentScore);
+    }
+  });
+
   $("#btn-hold").click(function(event) {
     event.preventDefault();
-  //  $(".ul-result").append("HOLD");
-  //  return("Hi");
+    Player1.Hold();
+    $(".total-score").text(Player1.currentScore);
   });
+
+  $("#btn-hold2").click(function(event) {
+    event.preventDefault();
+    Player2.Hold(Player2.totalScore);
+    $(".total-score2").text(Player2.currentScore);
+  });
+
 });
